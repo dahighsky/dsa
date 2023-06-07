6e7d339480381132a1b4fa086dee5fe5856ce809
dahighsky <aakashuttkarsh29@gmail.com>
2023-06-07 22:19:11 +0530

    please god please

--- binary_search_tree_recursive_5.cpp
+++ binary_search_tree_recursive_5.cpp
@@ -1,76 +1,99 @@
 /*
-Title: Write a menu driven C++ program to implement binary tree/binary search tree with n 
-nodes. 
-do following operation 
-1. Insert a node in a BST 
-2. delete a node from BST 
-3. Search node 
-4. All traversals (recursive) 
+Title: Write a menu driven C++ program to implement binary tree/binary search tree with n
+nodes.
+do following operation
+1. Insert a node in a BST
+2. delete a node from BST
+3. Search node
+4. All traversals (recursive)
 */
 
 #include <iostream>
 using namespace std;
 
-struct TreeNode {
+struct TreeNode
+{
     int data;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode *left;
+    TreeNode *right;
 };
 
-class BST {
+class BST
+{
 private:
-    TreeNode* root;
+    TreeNode *root;
 
 public:
-    BST() {
+    BST()
+    {
         root = nullptr;
     }
 
-    TreeNode* createNode(int value) {
-        TreeNode* newNode = new TreeNode();
+    TreeNode *createNode(int value)
+    {
+        TreeNode *newNode = new TreeNode();
         newNode->data = value;
         newNode->left = nullptr;
         newNode->right = nullptr;
         return newNode;
     }
 
-    TreeNode* insertNode(TreeNode* node, int value) {
-        if (node == nullptr) {
+    TreeNode *insertNode(TreeNode *node, int value)
+    {
+        if (node == nullptr)
+        {
             return createNode(value);
         }
 
-        if (value < node->data) {
+        if (value < node->data)
+        {
             node->left = insertNode(node->left, value);
-        } else if (value > node->data) {
+        }
+        else if (value > node->data)
+        {
             node->right = insertNode(node->right, value);
         }
 
         return node;
     }
 
-    TreeNode* deleteNode(TreeNode* node, int value) {
-        if (node == nullptr) {
+    TreeNode *deleteNode(TreeNode *node, int value)
+    {
+        if (node == nullptr)
+        {
             return nullptr;
         }
 
-        if (value < node->data) {
+        if (value < node->data)
+        {
             node->left = deleteNode(node->left, value);
-        } else if (value > node->data) {
+        }
+        else if (value > node->data)
+        {
             node->right = deleteNode(node->right, value);
-        } else {
-            if (node->left == nullptr && node->right == nullptr) {
+        }
+        else
+        {
+            if (node->left == nullptr && node->right == nullptr)
+            {
                 delete node;
                 node = nullptr;
-            } else if (node->left == nullptr) {
-                TreeNode* temp = node;
+            }
+            else if (node->left == nullptr)
+            {
+                TreeNode *temp = node;
                 node = node->right;
                 delete temp;
-            } else if (node->right == nullptr) {
-                TreeNode* temp = node;
+            }
+            else if (node->right == nullptr)
+            {
+                TreeNode *temp = node;
                 node = node->left;
                 delete temp;
-            } else {
-                TreeNode* temp = findMin(node->right);
+            }
+            else
+            {
+                TreeNode *temp = findMin(node->right);
                 node->data = temp->data;
                 node->right = deleteNode(node->right, temp->data);
             }
@@ -79,77 +102,103 @@ public:
         return node;
     }
 
-    TreeNode* findMin(TreeNode* node) {
-        while (node->left != nullptr) {
+    TreeNode *findMin(TreeNode *node)
+    {
+        while (node->left != nullptr)
+        {
             node = node->left;
         }
         return node;
     }
 
-    TreeNode* searchNode(TreeNode* node, int value) {
-        if (node == nullptr || node->data == value) {
+    TreeNode *searchNode(TreeNode *node, int value)
+    {
+        if (node == nullptr || node->data == value)
+        {
             return node;
         }
 
-        if (value < node->data) {
+        if (value < node->data)
+        {
             return searchNode(node->left, value);
-        } else {
+        }
+        else
+        {
             return searchNode(node->right, value);
         }
     }
 
-    void inorderTraversal(TreeNode* node) {
-        if (node != nullptr) {
+    void inorderTraversal(TreeNode *node)
+    {
+        if (node != nullptr)
+        {
             inorderTraversal(node->left);
             cout << node->data << " ";
             inorderTraversal(node->right);
         }
     }
 
-    void preorderTraversal(TreeNode* node) {
-        if (node != nullptr) {
+    void preorderTraversal(TreeNode *node)
+    {
+        if (node != nullptr)
+        {
             cout << node->data << " ";
             preorderTraversal(node->left);
             preorderTraversal(node->right);
         }
     }
 
-    void postorderTraversal(TreeNode* node) {
-        if (node != nullptr) {
+    void postorderTraversal(TreeNode *node)
+    {
+        if (node != nullptr)
+        {
             postorderTraversal(node->left);
             postorderTraversal(node->right);
             cout << node->data << " ";
         }
     }
 
-    void insert(int value) {
+    void insert(int value)
+    {
         root = insertNode(root, value);
         cout << "Node inserted successfully." << endl;
     }
 
-    void remove(int value) {
-        TreeNode* node = searchNode(root, value);
-        if (node == nullptr) {
+    void remove(int value)
+    {
+        TreeNode *node = searchNode(root, value);
+        if (node == nullptr)
+        {
             cout << "Node not found." << endl;
-        } else {
+        }
+        else
+        {
             root = deleteNode(root, value);
             cout << "Node deleted successfully." << endl;
         }
     }
 
-    void search(int value) {
-        TreeNode* node = searchNode(root, value);
-        if (node == nullptr) {
+    void search(int value)
+    {
+        TreeNode *node = searchNode(root, value);
+        if (node == nullptr)
+        {
             cout << "Node not found." << endl;
-        } else {
+        }
+        else
+        {
             cout << "Node found." << endl;
         }
     }
 
-    void traverse() {
-        if (root == nullptr) {
+    void traverse()
+    {
+        if (root == nullptr)
+        {
             cout << "Tree is empty." << endl;
-        } else {
+        }
+        else
+        {
             cout << "Inorder Traversal: ";
             inorderTraversal(root);
             cout << endl;
@@ -165,11 +214,13 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     BST bst;
     int choice, value;
 
-    do {
+    do
+    {
         cout << "----- Binary Search Tree Operations -----" << endl;
         cout << "1. Insert a node" << endl;
         cout << "2. Delete a node" << endl;
@@ -179,35 +230,36 @@ int main() {
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice) {
-            case 1:
-                cout << "Enter the value to insert: ";
-                cin >> value;
-                bst.insert(value);
-                break;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to insert: ";
+            cin >> value;
+            bst.insert(value);
+            break;
 
-            case 2:
-                cout << "Enter the value to delete: ";
-                cin >> value;
-                bst.remove(value);
-                break;
+        case 2:
+            cout << "Enter the value to delete: ";
+            cin >> value;
+            bst.remove(value);
+            break;
 
-            case 3:
-                cout << "Enter the value to search: ";
-                cin >> value;
-                bst.search(value);
-                break;
+        case 3:
+            cout << "Enter the value to search: ";
+            cin >> value;
+            bst.search(value);
+            break;
 
-            case 4:
-                bst.traverse();
-                break;
+        case 4:
+            bst.traverse();
+            break;
 
-            case 5:
-                cout << "Exiting the program..." << endl;
-                break;
+        case 5:
+            cout << "Exiting the program..." << endl;
+            break;
 
-            default:
-                cout << "Invalid choice. Please enter a valid option." << endl;
+        default:
+            cout << "Invalid choice. Please enter a valid option." << endl;
         }
 
         cout << endl;
